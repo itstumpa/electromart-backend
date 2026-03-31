74ecdb489ebce7359c1cbb8f13e8eef8d863385b
itstumpa <tumpadas1fiverr@gmail.com>
2026-03-31 17:36:08 +0600

    update 3 files and delete 2 files

--- jest.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  preset: 'ts-jest',           // ← this teaches Jest to read TypeScript
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],  // look for .test.ts files
-  forceExit: true,
-};
