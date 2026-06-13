aed2b361b96b8ff6b44db8c459a7a0f25587837a
itstumpa <tumpadas1fiverr@gmail.com>
2026-06-13 20:12:27 +0600

    added laderboard in admin modules

--- src/app/modules/leaderboard/leaderboard.routes.ts
+++ src/app/modules/leaderboard/leaderboard.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import { getVendorLeaderboard } from "./leaderboard.controller";
+import { authenticate } from "../../middlewares/authenticate";
+import { authorize } from "../../middlewares/authorize";
 
 const router = Router();
-router.get("/", getVendorLeaderboard); // public
+
+router.use(authenticate);
+router.use(authorize("ADMIN", "SUPER_ADMIN"));
+
+router.get("/", getVendorLeaderboard);
 
 export const leaderboardRoute = router;
